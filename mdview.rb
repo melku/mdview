2e80c656081ecd65ec63cbb6eb405a40a8f1d8db
melchior <melchior.corgie@gmail.com>
2014-08-31 00:26:33 -0400

    Handle unknown pages
    404 errors are handled by showing the contents of the directory.
    Doesn't work with nested directories, but the goal is to have an
    entry point. The navigation should be handled by the markdown
    pages.

--- mdview.rb
+++ mdview.rb
@@ -10,7 +10,18 @@ get '/' do
   redirect '/' + ( ARGV[0] or 'index.md' )
 end
 
-# read markdown pages
+# Read markdown pages
 get '/:page.md' do
   markdown params[:page].to_sym
+end
+
+# Handle unknown requests
+not_found do
+  status 404
+  pages = '<p>Page not found! Here are the pages you can try:<p><ul>'
+  Dir[File.join settings.views, '*.md'].each do |file|
+    pages << "<li><a href=\"#{File.basename file}\">#{File.basename file}</a></li>"
+  end
+  pages << '</ul>'
+  erb pages
 end
